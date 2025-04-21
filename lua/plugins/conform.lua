2f99a45f848b5154e32bea7d6492982c4f0f78f0
Ousmane <ousmane.209@gmail.com>
2025-04-21 19:20:02 +0200

    fix: update ts/js formatting setup using conform.nvim

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -19,6 +19,13 @@ return {
       lua = { 'stylua' },
       python = { 'black' },
       javascript = { 'prettierd', 'prettier', stop_after_first = true },
+      typescript = { 'prettierd', 'prettier', stop_after_first = true },
+      typescriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+      javascriptreact = { 'prettierd', 'prettier', stop_after_first = true },
+      json = { 'prettierd', 'prettier', stop_after_first = true },
+      html = { 'prettierd', 'prettier', stop_after_first = true },
+      css = { 'prettierd', 'prettier', stop_after_first = true },
+      scss = { 'prettierd', 'prettier', stop_after_first = true },
     },
   },
 }
